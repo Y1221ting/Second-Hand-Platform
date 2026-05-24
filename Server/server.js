2b8a40c0a856bb03f56c6ab021ad724ae3e6bce8
windy <yangting_1221@outlook.com>
2026-05-24 13:48:34 +0800

    fix: 移除密码返回、body-parser限流、密钥移入.env

--- Server/server.js
+++ Server/server.js
@@ -14,8 +14,8 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(bodyParser.json({ limit: "10mb" }));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
 app.use(express.json());
 app.use(
   cors({
